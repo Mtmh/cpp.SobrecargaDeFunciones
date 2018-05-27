2d989661f0c9bbdb4061fc32f931c3d604103f01
Tiziano Martin hernando <tizianomartinhernando@MacBook-Pro-de-Tiziano.local>
2018-05-27 12:28:28 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int suma(int a, int b)
+{
+    cout << "int" << endl;
+    return a+b;
+}
+
+float suma(float a , float b)
+{
+    cout << "float" << endl;
+    return a + b;
+}
+
+
+int main()
+{
+    cout << suma(1.0f, 2.0f) << endl;
+
+    return 0;
+}
+
+// lo que cambia son los tipos
